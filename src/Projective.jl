ea8c0e08c50ddbc7710e639a0963d38767c61cb3
Ed Scheinerman <ers@jhu.edu>
2015-01-01 22:21:04 -0500

    Added functionality

--- src/Projective.jl
+++ src/Projective.jl
@@ -1,8 +1,8 @@
-# For dealing with projective planes modulo a prime
+# For dealing with projective planes of prime order
 
 using Mods
 
-import Base.show
+import Base.show, Base.hash
 
 immutable Projective
     a::Mod
@@ -15,18 +15,21 @@ immutable Projective
         a = Mod(aa,p)
         b = Mod(bb,p)
         c = Mod(cc,p)
-        if a != 0
-            b *= a'
-            c *= a'
+
+        if c != 0
+            cinv = c'
+            a *= cinv
+            b *= cinv
+            c  = Mod(1,p)
+        elseif b != 0 # so c==0
+            a *= b'
+            b  = Mod(1,p)
+        elseif a != 0
             a = Mod(1,p)
-        elseif b != 0
-            c *= b'
-            b = Mod(1,p)
-        elseif c != 0
-            c = Mod(1,p)
         else
-            error("Values a,b,c cannot all be 0 in the given mod")
+            error("Values a,b,c cannot all be 0 mod " * string(p))
         end
+
         return new(a,b,c)
     end
 end
@@ -37,3 +40,58 @@ function show(io::IO, P::Projective)
 end
 
 *(P::Projective, Q::Projective) = P.a*Q.a + P.b*Q.b + P.c*Q.c
+
+incident(P::Projective, Q::Projective) = P*Q==0
+
+function hash(P::Projective, h::Uint64 = uint64(0))
+    h1::Uint64 = hash(P.a,h)
+    h2::Uint64 = hash(P.b,h1)
+    h3::Uint64 = hash(P.c,h2)
+    return h3
+end
+
+getvals(P::Projective) = (P.a.val, P.b.val, P.c.val, P.a.mod)
+
+function generate(p::Integer)
+    if ~isprime(p)
+        error("Modulus must be prime")
+    end
+    n = p*p+p+1
+    A = Array(Projective,n)
+    count = 1
+
+    # case a = 1
+    for b=0:p-1
+        for c = 0:p-1
+            A[count] = Projective(1,b,c,p)
+            count += 1
+        end
+    end
+    
+    # case a=0, b=1
+    for c=0:p-1       
+        A[count] = Projective(0,1,c,p)
+        count += 1
+    end
+
+    # case a=b=0, c=1
+    A[count] = Projective(0,0,1,p)
+
+    return A
+end
+
+function incidence_matrix(p::Integer)
+    pts = generate(p)
+    n = p*p + p + 1
+    A = zeros(Int,n,n)
+    for u = 1:n
+        P = pts[u]
+        for v = 1:n
+            Q = pts[v]
+            if incident(P,Q)
+                A[u,v] = 1
+            end
+        end
+    end
+    return A
+end
