039277618769a29f647da425ab0b6b3b1c95b2a1
Ed Scheinerman <ers@jhu.edu>
2015-11-21 11:10:59 -0500

    Updates for Zimmerman problem

---
+++ src/zimmertest.jl
@@ -0,0 +1,38 @@
+push!(LOAD_PATH,pwd())
+using Zimmermann
+
+
+front = collect(1:10)
+middle = collect(11:15)
+tail = collect(16:30)
+k = 20
+
+
+function try_both(front, middle, tail, k)
+
+    ultra = [ front; middle; tail]
+    n = length(ultra)
+
+    if IntSet(ultra) != IntSet(collect(1:n))
+        warn("front/middle/tail is not 1:", n)
+    end
+    
+    println("Considering ", binomial(n,k), " subsets of size ", k,
+            " of 1:", n)
+
+    println()
+    println("Parallel execution")
+    tic();
+    A = super_try_all(front, middle, tail, k)
+    println(A,"\t", length(sumprod(A)))
+    toc();
+    
+    println()
+    println("Serial execution")
+    tic()
+    B = try_all([middle;tail], k, front,false)
+    println(B,"\t", length(sumprod(B)))
+    toc()
+end
+
+try_both(front, middle, tail, k)
