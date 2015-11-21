abec0b6b9fd9735463952dee1c5944a197968946
Ed Scheinerman <ers@jhu.edu>
2015-11-21 14:29:24 -0500

    Parallel control file

---
+++ src/zimrun.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+# run this script with "sbatch zimrun.sh"
+
+#SBATCH
+#SBATCH --job-name=Zimmer
+#SBATCH --time=10
+#SBATCH --partition=parallel
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=24
+
+
+time julia -p 40 zimmertest.jl > harm.out
